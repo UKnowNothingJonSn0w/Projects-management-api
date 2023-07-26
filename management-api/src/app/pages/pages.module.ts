42ac3cc876a297a729174a78256f5fb80e1fbf65
UKnowNothingJonSn0w <wojcieszako@gmail.com>
2023-07-26 11:27:38 +0200

    main-site

---
+++ management-api/src/app/pages/pages.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { PagesRoutingModule } from './pages-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { BsModalService, BsModalRef, ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+//import { FlexLayoutModule } from '@angular/flex-layout';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    HomeComponent,
+   
+  ],
+
+
+  imports: [
+    ModalModule.forRoot(),
+    PagesRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    NgbModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    LayoutModule,
+    MatExpansionModule
+  ],
+
+  providers: [],
+  bootstrap: [PagesComponent],
+})
+export class PagesModule {}
