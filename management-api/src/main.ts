0a87e42e2b2118d562328383754b8fbb99aa757f
UKnowNothingJonSn0w <wojcieszako@gmail.com>
2023-08-09 16:40:39 +0200

    environment

--- management-api/src/main.ts
+++ management-api/src/main.ts
@@ -1,9 +1,14 @@
 /// <reference types="@angular/localize" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from './app/environments/environment';
+import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app/app.module';
 
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
